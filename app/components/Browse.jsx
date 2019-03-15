56c48157dd4eaa42cd47ffb510b02c745e06184b
heytitle <pat.chormai@gmail.com>
2019-03-15 20:33:30 +0700

    update browsing page

--- app/components/Browse.jsx
+++ app/components/Browse.jsx
@@ -21,63 +21,51 @@ const parties = availableParties.map(p => {
     }
 });
 
-class PartyList extends React.Component {
+class Browse extends React.Component {
     render() {
-        return <div className={style.container}>
-            {isSmallScreen() &&
-                <div className={style.msgModal}>
-                    <div className={style.msgModalBackground} />
-                    <div className={style.msgModalText}>
-                        งานชิ้นนี้ เหมาะสำหรับใช้บนจอขนาดใหญ่ <br />(ขนาดความกว้าง 1280px ขึ้นไป)
+        return (
+            <div className={style.background}>
+                <div className={style.container}>
+                    {isSmallScreen() &&
+                        <div className={style.msgModal}>
+                            <div className={style.msgModalBackground} />
+                            <div className={style.msgModalText}>
+                                งานชิ้นนี้ เหมาะสำหรับใช้บนจอขนาดใหญ่ <br />(ขนาดความกว้าง 1280px ขึ้นไป)
                         <br />
+                                <br />
+                                ขออภัยในความไม่สะดวก
                         <br />
-                        ขออภัยในความไม่สะดวก
-                        <br />
-                        <br />
-                        🙇🏻‍🙏🏼🙇🏻‍♀️
+                                <br />
+                                🙇🏻‍🙏🏼🙇🏻‍♀️
+                    </div>
+                        </div>
+                    }
+                    <h1>ผู้สมัครส.ส.แต่ละพรรค <br /> มีประวัติเกี่ยวข้องกับธุรกิจอะไรบ้าง</h1>
+                    <div className={style.selectContainer}>
+                        <Select
+                            autoFocus={true}
+                            className={style.selectElement}
+                            classNamePrefix="react-select"
+                            options={parties}
+                            placeholder="เลือกพรรค"
+                            onChange={(opt) => this.props.history.push(`/p/${opt.value}`)}
+                            getOptionLabel={({ icon, label }) => {
+                                return (
+                                    <div>
+                                        <div className={style.partyLogoContainer}>
+                                            <img
+                                                className={style.partyLogo}
+                                                src={icon}
+                                            />
+                                        </div>
+                                        <div className={style.partyName}>{label}</div>
+                                    </div>
+                                )
+                            }}
+                        />
                     </div>
                 </div>
-            }
-            <h1>ผู้สมัครส.ส.แต่ละพรรค <br /> มีประวัติเกี่ยวข้องกับธุรกิจอะไรบ้าง</h1>
-            <div className={style.selectContainer}>
-                <Select
-                    className={style.selectElement}
-                    classNamePrefix="react-select"
-                    options={parties}
-                    placeholder="เลือกพรรค"
-                    onChange={(opt) => this.props.history.push(`/p/${opt.value}`)}
-                    getOptionLabel={({icon, label}) => {
-                        return (
-                            <div>
-                                <img
-                                    className={style.partyLogo}
-                                    src={icon}
-                                /> 
-                                <div className={style.partyName}>{label}</div>
-                            </div>
-                        )
-                    }}
-                />
-            </div>
-
-
-
-            {/* <ul className={style.logoContainer}>
-                {
-                    availableParties.map(p => {
-                        p = p.trim();
-                        return <li>
-                            <Link to={`/p/${p}`}>
-                                <img
-                                    className={style.partyLogo}
-                                    src={}
-                                />
-                            </Link>
-                        </li>
-                    })
-                }
-            </ul> */}
-        </div>
+            </div>)
     }
 }
-export default PartyList
+export default Browse
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,35 @@
-var isProd         = process.env.NODE_ENV === 'production'
-  , webpack        = require('webpack')
-  , path           = require('path')
-  , autoprefixer   = require('autoprefixer')
-  , csswring       = require('csswring')
-  , mqpacker       = require('css-mqpacker')
-  , values         = require('postcss-modules-values')
-  , postcss_nested = require('postcss-nested')
-  , postcss_color  = require('postcss-color-function')
-  , package        = require('./package.json')
+var isProd = process.env.NODE_ENV === 'production',
+  webpack = require('webpack'),
+  path = require('path'),
+  autoprefixer = require('autoprefixer'), csswring = require('csswring'),
+  mqpacker = require('css-mqpacker'),
+  values = require('postcss-modules-values'),
+  postcss_nested = require('postcss-nested'),
+  postcss_color = require('postcss-color-function'),
+  package = require('./package.json')
 
-var ExtractTextPlugin = require('extract-text-webpack-plugin')
-  , HtmlWebpackPlugin = require('html-webpack-plugin')
-  , CopyWebpackPlugin = require('copy-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin'),
+  HtmlWebpackPlugin = require('html-webpack-plugin'),
+  CopyWebpackPlugin = require('copy-webpack-plugin');
 
 var cssLoaders = 'style!css?modules!postcss'
 
-const copyPlugin = new CopyWebpackPlugin([
-  { from: 'app/assets', to: 'assets' }
-])
+const copyPlugin = new CopyWebpackPlugin([{
+  from: 'app/assets',
+  to: 'assets'
+}])
 
 const definePlugin = new webpack.DefinePlugin({
   'process.env.publicPath': JSON.stringify(process.env.PUBLIC_PATH || '127.0.0.1:8080')
 })
 
 function extract(loaders) {
-  return ExtractTextPlugin.extract('style',  loaders.substr(loaders.indexOf('!')))
+  return ExtractTextPlugin.extract('style', loaders.substr(loaders.indexOf('!')))
 }
 
 var entry = isProd ? {
   app: './app/index.jsx',
-  vendors: [ 'react', 'react-router' ] //, 'lodash']
+  vendors: ['react', 'react-router'] //, 'lodash']
 } : {
   app: [
     'webpack-dev-server/client?http://0.0.0.0:8080',
@@ -39,47 +39,62 @@ var entry = isProd ? {
 }
 
 module.exports = {
-  debug: !isProd
-, devtool: 'eval'
-, entry: entry
+  debug: !isProd,
+  devtool: 'eval',
+  entry: entry
 
-, output: {
-    path: './dist'
-  , filename: isProd ? '[name].[chunkhash].js' : 'app.js'
-  , chunkFilename: isProd ? '[chunkhash].js' : '[id].js'
+    ,
+  output: {
+    path: './dist',
+    filename: isProd ? '[name].[chunkhash].js' : 'app.js',
+    chunkFilename: isProd ? '[chunkhash].js' : '[id].js'
   }
 
-, module: {
-    loaders: [
-      {
+  ,
+  module: {
+    loaders: [{
         test: /\.jsx?$/,
-        loaders: (isProd ? [] : [ 'react-hot' ]).concat([ 'babel' ]),
+        loaders: (isProd ? [] : ['react-hot']).concat(['babel']),
         exclude: /node_modules/
+      }, {
+        test: /\.css$/,
+        loader: isProd ? extract(cssLoaders) : cssLoaders
+      }, {
+        test: /\.png$/,
+        loader: "url?limit=100000&mimetype=image/png"
+      },
+      {
+        test: /\.svg$/,
+        loader: 'file-loader'
+      }, , {
+        test: /\.gif$/,
+        loader: "url?limit=100000&mimetype=image/gif"
+      }, {
+        test: /\.(png|woff|woff2|eot|ttf)$/,
+        loader: 'url-loader?limit=100000'
+      }, {
+        test: /\.jpg$/,
+        loader: "file"
       }
-    , {test: /\.css$/, loader: isProd ? extract(cssLoaders) : cssLoaders}
-    , {test: /\.png$/, loader: "url?limit=100000&mimetype=image/png"}
-    , {test: /\.svg$/, loader: "url?limit=100000&mimetype=image/svg+xml"}
-    , {test: /\.gif$/, loader: "url?limit=100000&mimetype=image/gif"}
-    , {test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000'}
-    , {test: /\.jpg$/, loader: "file"}
     ]
   }
 
-, postcss: function() {
-    return [
-        values
-      , postcss_nested
-      , postcss_color
-      , autoprefixer
-      , mqpacker
-      , csswring
-    ]
-  }
+  ,
+  postcss: function () {
+      return [
+        values, postcss_nested, postcss_color, autoprefixer, mqpacker, csswring
+      ]
+    }
 
-, plugins: isProd ? [
+    ,
+  plugins: isProd ? [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false } }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
     new webpack.optimize.CommonsChunkPlugin('vendors', '[name].[hash].js'),
     new ExtractTextPlugin('[name].[hash].css'),
     new HtmlWebpackPlugin({
@@ -97,9 +112,9 @@ module.exports = {
     }),
     copyPlugin,
     definePlugin
-  ] 
-, resolve: {
-    modulesDirectories: [ 'app', 'node_modules' ]
-  , extensions: ['', '.js', '.json', '.jsx', '.css']
+  ],
+  resolve: {
+    modulesDirectories: ['app', 'node_modules'],
+    extensions: ['', '.js', '.json', '.jsx', '.css']
   }
 };
