c680e9e0bc1f6841d414f35e64ae72a32ef73599
heytitle <pat.chormai@gmail.com>
2019-03-19 12:07:21 +0700

    update deploy path

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-export GIT_DEPLOY_REPO=git@github.com:heytitle/politician-businesss.git
-export GIT_DEPLOY_BRANCH=gh-pages
+ENV=$1
+
 export GIT_DEPLOY_DIR=dist 
 
-echo "Building and deploying for $1"
+if [ "$ENV" == "stage" ]; then
+    export GIT_DEPLOY_REPO=git@github.com:heytitle/politician-businesss.git
+    export GIT_DEPLOY_BRANCH=gh-pages
+elif [ "$ENV" == "prod" ]; then
+    export GIT_DEPLOY_REPO=git@gitlab.com:elect.in.th/politician-business.git
+    export GIT_DEPLOY_BRANCH=master
+else
+   echo "Unknown environment $ENV"
+   exit
+fi
+
+echo "Building and deploying for $ENV ($GIT_DEPLOY_REPO)"
 
 npm run build:$1 \
     && ./scripts/push-page.sh
