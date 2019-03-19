7a845b379ba3fe498381436a508196bf860f9551
heytitle <pat.chormai@gmail.com>
2019-03-19 12:26:18 +0700

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,13 +9,14 @@ if [ "$ENV" == "stage" ]; then
     export GIT_DEPLOY_BRANCH=gh-pages
 elif [ "$ENV" == "prod" ]; then
     export GIT_DEPLOY_REPO=git@gitlab.com:elect.in.th/politician-business.git
-    export GIT_DEPLOY_BRANCH=master
+    export GIT_DEPLOY_BRANCH=deploy
 else
    echo "Unknown environment $ENV"
    exit
 fi
 
-echo "Building and deploying for $ENV ($GIT_DEPLOY_REPO)"
+echo "Building and deploying for $ENV ($GIT_DEPLOY_REPO, $GIT_DEPLOY_BRANCH)"
 
 npm run build:$1 \
-    && ./scripts/push-page.sh
+    && GIT_DEPLOY_REPO=$GIT_DEPLOY_REPO GIT_DEPLOY_BRANCH=$GIT_DEPLOY_BRANCH \
+       GIT_DEPLOY_DIR=$GIT_DEPLOY_DIR ./scripts/push-page.sh
